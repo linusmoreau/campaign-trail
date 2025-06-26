a3bd2d9941404d7e6190666c58ba95d46ddbe514
Linus Moreau <linus.moreau@gmail.com>
2025-06-25 20:31:37 -0700

    Make election map and results consistent

--- src/generate_code_2.py
+++ src/generate_code_2.py
@@ -3,6 +3,8 @@ import json
 import pandas as pd
 from collections import OrderedDict
 from xml.dom import minidom
+import sys
+sys.stdout.reconfigure(encoding='utf-8') # type: ignore
 
 class Code2Generator:
     ELECTION_VARIABLE_NAMES = (
@@ -27,6 +29,10 @@ class Code2Generator:
         self.scenario_name = scenario_name
         self.election_dir = os.path.join(os.pardir, election_name)
         self.scenario_dir = os.path.join(os.pardir, scenario_name)
+        
+    def generate(self):
+        code2 = self.join_sections()
+        self.save_to_file(code2)
 
     def complete_map(self) -> str:
         with open(os.path.join(self.election_dir, "states.json"), "r") as f:
@@ -70,35 +76,53 @@ class Code2Generator:
         output_filepath = os.path.join(self.scenario_dir, self.scenario_name + ".html")
         with open(output_filepath, "w") as f:
             f.write(s)
-            
-    def generate(self):
-        code2 = self.join_sections()
-        self.save_to_file(code2)
         
-    def extract_geometry(self):
+    def extract_geometry(self) -> pd.DataFrame:
         doc = minidom.parse(os.path.join(self.election_dir, "election_map.svg"))
         path_geometries = [
-            {
-                "name": path.getAttribute("inkscape:label"),
-                "d": path.getAttribute("d")
-            }
+            (
+                path.getAttribute("inkscape:label"),
+                path.getAttribute("d"),
+            )
             for path in doc.getElementsByTagName('path')
         ]
-        return path_geometries[:343]
+        return pd.DataFrame(path_geometries[:343], columns=("name", "d"))
     
-    def extract_historical_results(self):
+    def extract_historical_results(self) -> pd.DataFrame:
         path = os.path.join(self.election_dir, "election_results.txt")
         df = pd.read_csv(path, sep='\t', header=1)
-        print(len(df))
         df = df[df["Type of results*"] == "validated"]
-        print(len(df))
-        df = df[df["Political affiliation"] == "NDP-New Democratic Party"]
-        print(len(df))
-        pass
+        df = df.rename(columns={
+            "Electoral district name": "name",
+            "Political affiliation": "party",
+            "Votes obtained - Votes obtenus": "votes",
+            "Total number of ballots cast - Nombre total de votes déposés": "total"
+        })
+        df = df[["name", "party", "votes", "total"]]
+        return df
+    
+    def check_ridings_match(self, results, geometry):
+        results_ridings = set(results["name"])
+        geometry_ridings = set(geometry["name"])
+        mismatch = results_ridings.union(geometry_ridings).difference(results_ridings.intersection(geometry_ridings))
+        if len(mismatch) > 0:
+            print("Warning: mismatch in ridings named in election results and map:", mismatch)
+    
+    def get_riding_data(self):
+        results = self.extract_historical_results()
+        geometry = self.extract_geometry()
+        self.check_ridings_match(results, geometry)
+        
+        
+        
+        
+        
 
 if __name__ == "__main__":
     election_name = "2025_Canada"
     scenario_name = "2025_LiberalCarney"
     generator = Code2Generator(election_name, scenario_name)
-    generator.generate()
+    print(generator.get_riding_data())
+    # generator.generate()
     # generator.extract_historical_results()
+    # print(generator.extract_geometry())
