021edfd5ac14a354ccbb23aca7dcac33f67adffd
Linus Moreau <linus.moreau@gmail.com>
2025-06-27 15:56:56 -0700

    Adding scaling and filtering to svg compression

--- src/svg_compress.py
+++ src/svg_compress.py
@@ -1,4 +1,5 @@
 import os
+import math
 from xml.dom import minidom
 import svg.path as svg
 import rdp
@@ -6,31 +7,42 @@ import rdp
 def to_complex(n: tuple[float, float]):
     return complex(n[0], n[1])
 
-def compress_svg_path(d: str, epsilon: float = 0) -> str:
+def to_tuple(n: complex):
+    return (n.real, n.imag)
+
+def compress_svg_path(d: str, epsilon: float = 0, scale_epsilon: bool = True, min_length: float = 0) -> str:
     path = svg.parse_path(d)
-    point_sequences = []
-    point_sequence = []
-    for segment in path:
+    section_indices = []
+    section_start = 0
+    for i, segment in enumerate(path):
         if type(segment) not in (svg.Move, svg.Line, svg.Close):
             return d
-        else:
-            x = segment.end.real
-            y = segment.end.imag
-            point_sequence.append((x, y))
         if type(segment) is svg.Close:
-            point_sequences.append(point_sequence)
-            point_sequence = []
+            section_indices.append((section_start, i))
+            section_start = i + 1
+    lengths = [svg.Path(*path[ends[0]:ends[1]+1]).length() for ends in section_indices]
+    sections = map(lambda ends: map(lambda segment: to_tuple(segment.end), path[ends[0]:ends[1]+1]), section_indices)
     new_path = svg.Path()
-    for point_sequence in point_sequences:
-        compressed = rdp.rdp(point_sequence, epsilon=epsilon) # type: ignore as the module incorrectly type hints int
+    for i, section in enumerate(sections):
+        path_section = list(section)
+        length = lengths[i]
+        if length is not None and length < min_length:
+            continue
+        if scale_epsilon:
+            if length is None:
+                eps = epsilon
+            else:
+                eps = epsilon*math.sqrt(length)
+        compressed = rdp.rdp(path_section, epsilon=eps) # type: ignore as the module incorrectly type hints int
+        print(len(path_section), len(compressed))
         new_path.append(svg.Move(to_complex(compressed[0])))
         for i in range(len(compressed) - 2):
-            new_path.append(svg.Line(to_complex(compressed[i]), to_complex(compressed[i+1])))  
-        new_path.append(svg.Close(to_complex(compressed[-2]), compressed[-1]))
+            new_path.append(svg.Line(to_complex(compressed[i]), to_complex(compressed[i+1])))
+        new_path.append(svg.Close(to_complex(compressed[-2]), to_complex(compressed[-1])))
     return new_path.d()
 
 
-def compress_svg_file(f_in: str, f_out: str | None = None, first: int = 0, count: int | None = None, epsilon: float = 0):
+def compress_svg_file(f_in: str, f_out: str | None = None, first: int = 0, count: int | None = None, epsilon: float = 0, scale_epsilon: bool = True, min_length: float = 0):
     """Compress an SVG file using the Ramer-Douglas-Peucker algorithm.
     
     :Keyword Arguments:
@@ -47,10 +59,13 @@ def compress_svg_file(f_in: str, f_out: str | None = None, first: int = 0, count
         paths = paths[:count]
     for path in paths:
         d = path.getAttribute("d")
-        d = compress_svg_path(d, epsilon)
+        d = compress_svg_path(d, epsilon=epsilon, scale_epsilon=True, min_length=min_length)
         path.setAttribute("d", d)
     if f_out is None:
         fname, ext = os.path.splitext(f_in)
         f_out = fname + "_compressed" + ext
     with open(f_out, "w", encoding="utf-8") as f:
         doc.writexml(f, encoding="utf-8")
+
+if __name__ == "__main__":
+    compress_svg_file("../2025_Canada/election_map.svg", count=343, epsilon=0.05, scale_epsilon=True, min_length=5)
