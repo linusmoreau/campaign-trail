948e981583657bbf173602e8c6cd0053c83c810e
Linus Moreau <linus.moreau@gmail.com>
2025-07-14 08:32:28 -0700

    Add VW algorithm option

--- src/svg_compress.py
+++ src/svg_compress.py
@@ -3,6 +3,7 @@ import math
 from xml.dom import minidom
 import svg.path as svg
 import rdp
+import visvalingamwyatt as vw
 
 def to_complex(n: tuple[float, float]):
     return complex(n[0], n[1])
@@ -10,45 +11,59 @@ def to_complex(n: tuple[float, float]):
 def to_tuple(n: complex):
     return (n.real, n.imag)
 
-def compress_svg_path(d: str, epsilon: float = 0, scale_epsilon: bool = True, min_length: float = 0) -> str:
+def parse_path(d: str):
     path = svg.parse_path(d)
     section_indices = []
     section_start = 0
     for i, segment in enumerate(path):
         if type(segment) not in (svg.Move, svg.Line, svg.Close):
-            return d
+            raise TypeError("Segment is not a polyline")
         if type(segment) is svg.Close:
             section_indices.append((section_start, i))
             section_start = i + 1
     lengths = [svg.Path(*path[ends[0]:ends[1]+1]).length() for ends in section_indices]
     sections = map(lambda ends: map(lambda segment: to_tuple(segment.end), path[ends[0]:ends[1]+1]), section_indices)
+    return sections, lengths
+
+def compress_svg_path(d: str, method: str = "vw", epsilon: float = 0, scale_epsilon: bool = False, min_length: float = 0) -> str:
+    try:
+        sections, lengths = parse_path(d)
+    except TypeError:
+        return d
     new_path = svg.Path()
     for i, section in enumerate(sections):
         path_section = list(section)
         length = lengths[i]
         if length is not None and length < min_length:
             continue
-        if scale_epsilon:
-            if length is None:
-                eps = epsilon
-            else:
-                eps = epsilon*math.sqrt(length)
-        compressed = rdp.rdp(path_section, epsilon=eps) # type: ignore as the module incorrectly type hints int
-        print(len(path_section), len(compressed))
+        if scale_epsilon and length is not None:
+            eps = epsilon*math.sqrt(length)
+        else:
+            eps = epsilon                
+        if method == "vw":
+            compressed = vw.simplify(path_section, threshold=eps)
+        elif method == "rdp":
+            compressed = rdp.rdp(path_section, epsilon=eps) # type: ignore as the module incorrectly type hints int
+        else:
+            raise NotImplementedError('method must be one of "vw" or "rdp"')
+        print(f"Compressed line of {len(path_section)} points to {len(compressed)} points.")
         new_path.append(svg.Move(to_complex(compressed[0])))
-        for i in range(len(compressed) - 2):
-            new_path.append(svg.Line(to_complex(compressed[i]), to_complex(compressed[i+1])))
+        for j in range(len(compressed) - 2):
+            new_path.append(svg.Line(to_complex(compressed[j]), to_complex(compressed[j+1])))
         new_path.append(svg.Close(to_complex(compressed[-2]), to_complex(compressed[-1])))
     return new_path.d()
 
 
-def compress_svg_file(f_in: str, f_out: str | None = None, first: int = 0, count: int | None = None, epsilon: float = 0, scale_epsilon: bool = True, min_length: float = 0):
-    """Compress an SVG file using the Ramer-Douglas-Peucker algorithm.
+def compress_svg_file(f_in: str, f_out: str | None = None, first: int = 0, count: int | None = None, method: str = "vw", epsilon: float = 0, scale_epsilon: bool = False, min_length: float = 0):
+    """Compress an SVG file using either the Visvalingam-Whyatt or Ramer-Douglas-Peucker algorithms.
     
     :Keyword Arguments:
     :f_in: path to input SVG file
     :f_out: path to output SVG file; defaults to same as f_in with suffixed with _compressed
-    :epsilon: epsilon from the RDP algorithm; higher values give stronger compression
+    :method: one of "vw" or "rdp"; specifies which algorithm to use
+    :epsilon: epsilon from the RDP algorithm or threshold in the VW algorithm; higher values give stronger compression
+    :scale_epsilon: whether the epsilon should scale with size
+    :min_length: minimum line length to be included in the compressed file
     :first: index of first path to compress
     :count: number of paths to compress; defaults to compressing to all subsequent paths
     """
@@ -59,7 +74,7 @@ def compress_svg_file(f_in: str, f_out: str | None = None, first: int = 0, count
         paths = paths[:count]
     for path in paths:
         d = path.getAttribute("d")
-        d = compress_svg_path(d, epsilon=epsilon, scale_epsilon=scale_epsilon, min_length=min_length)
+        d = compress_svg_path(d, method=method, epsilon=epsilon, scale_epsilon=scale_epsilon, min_length=min_length)
         path.setAttribute("d", d)
     if f_out is None:
         fname, ext = os.path.splitext(f_in)
@@ -68,4 +83,4 @@ def compress_svg_file(f_in: str, f_out: str | None = None, first: int = 0, count
         doc.writexml(f, encoding="utf-8")
 
 if __name__ == "__main__":
-    compress_svg_file("../2025_Canada/election_map.svg", count=343, epsilon=0.05, scale_epsilon=True, min_length=4)
+    compress_svg_file("../2025_Canada/election_map.svg", count=343, epsilon=0.2, scale_epsilon=True, min_length=4)
